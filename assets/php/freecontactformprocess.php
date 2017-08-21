f6920d3b6c8d3000085b6010099e003c4f172e70
Sean Mulhall <smulhall1337@gmail.com>
2017-08-21 02:47:02 -0400

    cleaned up some stuff. made some variables consistent

--- assets/php/freecontactformprocess.php
+++ assets/php/freecontactformprocess.php
@@ -20,79 +20,21 @@
  * 
  */
 
+echo "before/n";
+?>
+
+<pre>
+<?php var_dump($_POST); ?>
+</pre>
+
+<?php
+
+
 if(isset($_POST['Email_Address'])) {
 
+	echo "after first if";
 
-
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-
-	error_reporting(E_ALL);
-
-	// ----------------------------------------------------------------------------------------------------
-	// - Display Errors
-	// ----------------------------------------------------------------------------------------------------
-	ini_set('display_errors', 'On');
-	ini_set('html_errors', 0);
-
-	// ----------------------------------------------------------------------------------------------------
-	// - Error Reporting
-	// ----------------------------------------------------------------------------------------------------
-	error_reporting(-1);
-
-	// ----------------------------------------------------------------------------------------------------
-	// - Shutdown Handler
-	// ----------------------------------------------------------------------------------------------------
-	function ShutdownHandler()
-	{
-		if(@is_array($error = @error_get_last()))
-		{
-			return(@call_user_func_array('ErrorHandler', $error));
-		};
-
-		return(TRUE);
-	};
-
-	register_shutdown_function('ShutdownHandler');
-
-	// ----------------------------------------------------------------------------------------------------
-	// - Error Handler
-	// ----------------------------------------------------------------------------------------------------
-	function ErrorHandler($type, $message, $file, $line)
-	{
-		$_ERRORS = Array(
-			0x0001 => 'E_ERROR',
-			0x0002 => 'E_WARNING',
-			0x0004 => 'E_PARSE',
-			0x0008 => 'E_NOTICE',
-			0x0010 => 'E_CORE_ERROR',
-			0x0020 => 'E_CORE_WARNING',
-			0x0040 => 'E_COMPILE_ERROR',
-			0x0080 => 'E_COMPILE_WARNING',
-			0x0100 => 'E_USER_ERROR',
-			0x0200 => 'E_USER_WARNING',
-			0x0400 => 'E_USER_NOTICE',
-			0x0800 => 'E_STRICT',
-			0x1000 => 'E_RECOVERABLE_ERROR',
-			0x2000 => 'E_DEPRECATED',
-			0x4000 => 'E_USER_DEPRECATED'
-		);
-
-		if(!@is_string($name = @array_search($type, @array_flip($_ERRORS))))
-		{
-			$name = 'E_UNKNOWN';
-		};
-
-		return(print(@sprintf("%s Error in file \xBB%s\xAB at line %d: %s\n", $name, @basename($file), $line, $message)));
-	};
-
-	$old_error_handler = set_error_handler("ErrorHandler");
-
-
-
-	//	include 'freecontactformsettings.php';
-	$email_to = "smulhall1337@gmail.com";
-	$email_subject = "new contact";
+	include 'freecontactformsettings.php';
 
 	function died($error) {
 		echo "Sorry, but there were error(s) found with the form you submitted. ";
@@ -149,6 +91,8 @@ if(isset($_POST['Email_Address'])) {
 	$email_message .= "Telephone: ".clean_string($telephone)."\r\n";
 	$email_message .= "Message: ".clean_string($comments)."\r\n";
 
+	echo something;
+
 	$headers = 'From: '.$email_from."\r\n".
 		'Reply-To: '.$email_from."\r\n" .
 		'X-Mailer: PHP/' . phpversion();
@@ -156,7 +100,9 @@ if(isset($_POST['Email_Address'])) {
 	header("Location: $thankyou");
 ?>
 <script>location.replace('<?php echo $thankyou;?>')</script>
+
 <?php
 }
+echo "dead";
 die();
 ?>

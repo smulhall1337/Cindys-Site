13dabb131ed31068e7a2732ddb7624e6a3ef40b3
Sean Mulhall <smulhall1337@gmail.com>
2017-08-21 02:44:59 -0400

    removed php error handling, still attempting to get php email to work

---
+++ assets/php/freecontactformsettings.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$email_to = "smulhall1337@gmail.colm"; // your email address
+$email_subject = "Contact form submission"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+?>
